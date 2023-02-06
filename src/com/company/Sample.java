389fc60d0c0e2da6b48c910d46753904b1bf8fed
parpinelopi <pennyblack.p@gmail.com>
2023-02-06 07:39:32 +0100

    Add Sample class

--- src/com/company/Sample.java
+++ src/com/company/Sample.java
@@ -1,17 +1,24 @@
 package com.company;
 
+import java.util.Date;
+
+/**
+ *This class provides the sample information
+ */
 public class Sample {
     Laboratory laboratory ;
     Location location ;
     Control  control ;
     String quantity;
     int quantityUnit;
+    Date sampleDate;
 
-    public Sample(Laboratory laboratory, Location location, Control control, String quantity, int quantityUnit) {
+    public Sample(Laboratory laboratory, Location location, Control control, String quantity, int quantityUnit, Date sampleDate) {
         this.laboratory = laboratory;
         this.location = location;
         this.control = control;
         this.quantity = quantity;
         this.quantityUnit = quantityUnit;
+        this.sampleDate = sampleDate;
     }
 }
